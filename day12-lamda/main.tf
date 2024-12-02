b616b5c3c36222f2f91328b806c90049d54d3d8a
email <narasimhav654@gmail.com>
2024-12-02 20:03:56 +0530

    all

--- day12-lamda/main.tf
+++ day12-lamda/main.tf
@@ -12,8 +12,9 @@ resource "aws_s3_bucket" "lambda_bucket" {
 # Upload Lambda Code to S3
 resource "aws_s3_object" "lambda_code" {
   bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "lambda_function.zip"
+  key    = "C:/Users/DELL/Downloads/lambda_function.zip"
   source = "lambda_function.zip"
+  
    # Path to your zip file containing the Lambda code
   etag   = filemd5("lambda_function.zip")
 }
